854c534bec8e9a901894fea25ceef8bac5bd0d50
Talley Lambert <talley.lambert@gmail.com>
2020-03-13 12:57:33 -0400

    minor

--- napari-{{cookiecutter.plugin_name}}/tests/test_{{cookiecutter.module_name}}.py
+++ napari-{{cookiecutter.plugin_name}}/tests/test_{{cookiecutter.module_name}}.py
@@ -7,7 +7,8 @@ from napari_{{cookiecutter.module_name}} import napari_get_reader
 # def test_reader():
 #     from tempfile import NamedTemporaryFile
 #     import numpy as np
-
+#
+#     # use your own `.ext` here
 #     with NamedTemporaryFile(suffix='.ext', delete=False) as tmp:
 #         out_data = np.random.rand(20, 20)
 #         # write_data_to_file(tmp.name, out_data)
