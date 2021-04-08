5b29882a6d3b96abf2f668f99a65bf3644c7f202
Talley Lambert <talley.lambert@gmail.com>
2021-04-08 12:05:00 -0400

    use template for plugin name

--- {{cookiecutter.plugin_name}}/{{cookiecutter.module_name}}/_tests/test_dock_widget.py
+++ {{cookiecutter.plugin_name}}/{{cookiecutter.module_name}}/_tests/test_dock_widget.py
@@ -2,7 +2,7 @@ from {{cookiecutter.module_name}} import napari_experimental_provide_dock_widget
 import pytest
 
 # this is your plugin name declared in your napari.plugins entry point
-MY_PLUGIN_NAME = "foo-bar"
+MY_PLUGIN_NAME = "{{cookiecutter.plugin_name}}"
 # the name of your widget(s)
 MY_WIDGET_NAMES = ["Example Q Widget", "example_magic_widget"]
 
