dc0aa48273f25f6587cdc83ac516b5c588858d57
Talley Lambert <talley.lambert@gmail.com>
2021-04-21 06:09:05 -0400

    add dock widget smoke test (#21)
    * add dock widget smoke test
    
    * no fail fast
    
    * no -s
    
    * remove non ascii
    
    * delete
    
    * use template for plugin name
    
    * add linux libs

--- {{cookiecutter.plugin_name}}/{{cookiecutter.module_name}}/_dock_widget.py
+++ {{cookiecutter.plugin_name}}/{{cookiecutter.module_name}}/_dock_widget.py
@@ -8,9 +8,10 @@ Replace code below according to your needs.
 """
 from napari_plugin_engine import napari_hook_implementation
 from qtpy.QtWidgets import QWidget, QHBoxLayout, QPushButton
+from magicgui import magic_factory
 
 
-class MyWidget(QWidget):
+class ExampleQWidget(QWidget):
     # your QWidget.__init__ can optionally request the napari viewer instance
     # in one of two ways:
     # 1. use a parameter called `napari_viewer`, as done here
@@ -29,7 +30,12 @@ class MyWidget(QWidget):
         print("napari has", len(self.viewer.layers), "layers")
 
 
+@magic_factory
+def example_magic_widget(img_layer: "napari.layers.Image"):
+    print(f"you have selected {img_layer}")
+
 
 @napari_hook_implementation
 def napari_experimental_provide_dock_widget():
-    return MyWidget
+    # you can return either a single widget, or a sequence of widgets
+    return [ExampleQWidget, example_magic_widget]
--- {{cookiecutter.plugin_name}}/{{cookiecutter.module_name}}/_tests/test_dock_widget.py
+++ {{cookiecutter.plugin_name}}/{{cookiecutter.module_name}}/_tests/test_dock_widget.py
@@ -1,3 +1,17 @@
-# from {{cookiecutter.module_name}} import napari_experimental_provide_dock_widget
+from {{cookiecutter.module_name}} import napari_experimental_provide_dock_widget
+import pytest
 
-# add your tests here...
+# this is your plugin name declared in your napari.plugins entry point
+MY_PLUGIN_NAME = "{{cookiecutter.plugin_name}}"
+# the name of your widget(s)
+MY_WIDGET_NAMES = ["Example Q Widget", "example_magic_widget"]
+
+
+@pytest.mark.parametrize("widget_name", MY_WIDGET_NAMES)
+def test_something_with_viewer(widget_name, make_napari_viewer):
+    viewer = make_napari_viewer()
+    num_dw = len(viewer.window._dock_widgets)
+    viewer.window.add_plugin_dock_widget(
+        plugin_name=MY_PLUGIN_NAME, widget_name=widget_name
+    )
+    assert len(viewer.window._dock_widgets) == num_dw + 1
