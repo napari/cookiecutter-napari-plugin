44fc5a448e4b375f0b939907b5b97f71940ead5d
Draga Doncila Pop <ddon0001@student.monash.edu>
2021-05-11 10:07:53 +1000

    Remove install_requires from setup.py

--- {{cookiecutter.plugin_name}}/setup.py
+++ {{cookiecutter.plugin_name}}/setup.py
@@ -2,16 +2,6 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
-
-# Add your dependencies in requirements.txt
-# Note: you can add test-specific requirements in tox.ini
-requirements = []
-with open('requirements.txt') as f:
-    for line in f:
-        stripped = line.split("#")[0].strip()
-        if len(stripped) > 0:
-            requirements.append(stripped)
-
 {% if cookiecutter.plugin_name == "foo-bar" %}
 # extracted because it breaks testing of this cookiecutter template
 use_scm = False
@@ -20,6 +10,5 @@ use_scm = False
 use_scm = {"write_to": "{{cookiecutter.module_name}}/_version.py"}
 {% endif %}
 setup(
-    install_requires=requirements,
     use_scm_version=use_scm,
 )
