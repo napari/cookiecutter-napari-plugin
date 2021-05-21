07d67cd985b3b42e141dfbcdfe1a3d25f94fe55f
Talley Lambert <talley.lambert@gmail.com>
2021-05-21 09:29:39 -0400

    formatting

--- {{cookiecutter.plugin_name}}/setup.py
+++ {{cookiecutter.plugin_name}}/setup.py
@@ -2,16 +2,15 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
-{% if cookiecutter.plugin_name == "foo-bar" %}
+{% if cookiecutter.use_git_tags_for_versioning == 'y' -%}
+{% if cookiecutter.plugin_name == "foo-bar" -%}
 # extracted because it breaks testing of this cookiecutter template
 use_scm = False
 {% else %}
 # https://github.com/pypa/setuptools_scm
 use_scm = {"write_to": "{{cookiecutter.module_name}}/_version.py"}
-{% endif %}
-
-{% if cookiecutter.use_git_tags_for_versioning == 'y' %}
+{%- endif %}
 setup(use_scm_version=use_scm)
-{% else %}
+{%- else -%}
 setup()
-{% endif %}
+{%- endif %}
