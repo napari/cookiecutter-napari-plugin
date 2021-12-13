07ae9b3171183c1acad7bb45c4d464c7adf7b6a3
Nathan Clack <nclack@chanzuckerberg.com>
2021-12-12 18:28:08 -0800

    updates for schema changes

--- {{cookiecutter.plugin_name}}/src/{{cookiecutter.module_name}}/_tests/test_widget.py
+++ {{cookiecutter.plugin_name}}/src/{{cookiecutter.module_name}}/_tests/test_widget.py
@@ -4,7 +4,7 @@ import pytest
 # this is your plugin name declared in your napari.plugins entry point
 MY_PLUGIN_NAME = "{{cookiecutter.plugin_name}}"
 # the name of your widget(s)
-MY_WIDGET_NAMES = ["Example Q Widget", "example_magic_widget"]
+MY_WIDGET_NAMES = ["Example Q Widget", "Example Magic Widget","Example Function Widget"]
 
 
 @pytest.mark.parametrize("widget_name", MY_WIDGET_NAMES)
