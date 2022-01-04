05ffe4fea06b90e330fb98e7e8aa14ee1fa8d6f9
Draga Doncila Pop <ddoncila@gmail.com>
2022-01-04 11:40:58 +1000

    Remove gratuitous image fixture

--- {{cookiecutter.plugin_name}}/src/{{cookiecutter.module_name}}/_tests/test_dock_widget.py
+++ {{cookiecutter.plugin_name}}/src/{{cookiecutter.module_name}}/_tests/test_dock_widget.py
@@ -1,20 +1,12 @@
 from {{cookiecutter.module_name}} import ExampleQWidget, example_magic_widget
 import numpy as np
-import pytest
-
-# we create a fixture that returns us a random image array
-# we can then pass this fixture as an argument to our tests
-@pytest.fixture
-def random_im():
-    return np.random.random((100, 100))
 
 # make_napari_viewer is a pytest fixture that returns a napari viewer object
 # capsys is a pytest fixture that captures stdout and stderr output streams
-# random_im is the pytest fixture we defined above
-def test_example_q_widget(make_napari_viewer, capsys, random_im):
+def test_example_q_widget(make_napari_viewer, capsys):
     # make viewer and add an image layer using our fixture
     viewer = make_napari_viewer()
-    viewer.add_image(random_im)
+    viewer.add_image(np.random.random((100, 100)))
 
     # create our widget, passing in the viewer
     my_widget = ExampleQWidget(viewer)
@@ -26,9 +18,9 @@ def test_example_q_widget(make_napari_viewer, capsys, random_im):
     captured = capsys.readouterr()
     assert captured.out == "napari has 1 layers\n"
     
-def test_example_magic_widget(make_napari_viewer, capsys, random_im):
+def test_example_magic_widget(make_napari_viewer, capsys):
     viewer = make_napari_viewer()
-    layer = viewer.add_image(random_im)
+    layer = viewer.add_image(np.random.random((100, 100)))
 
     # this time, our widget will be a MagicFactory or FunctionGui instance
     my_widget = example_magic_widget()
