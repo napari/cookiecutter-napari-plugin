2020706c97c8ada82b96823ec7de1ab16fabd378
Draga Doncila Pop <17995243+DragaDoncila@users.noreply.github.com>
2022-01-05 02:53:50 +1000

    Tweak `dock_widget` tests to get rid of `add_plugin_dock_widget` (#82)
    * Update widget tests to forego use of add_dock_widget
    
    * Add explanatory comments
    
    * Remove gratuitous image fixture

--- {{cookiecutter.plugin_name}}/src/{{cookiecutter.module_name}}/_tests/test_dock_widget.py
+++ {{cookiecutter.plugin_name}}/src/{{cookiecutter.module_name}}/_tests/test_dock_widget.py
@@ -1,18 +1,33 @@
-import {{cookiecutter.module_name}}
-import pytest
+from {{cookiecutter.module_name}} import ExampleQWidget, example_magic_widget
+import numpy as np
 
-# this is your plugin name declared in your napari.plugins entry point
-MY_PLUGIN_NAME = "{{cookiecutter.plugin_name}}"
-# the name of your widget(s)
-MY_WIDGET_NAMES = ["Example Q Widget", "example_magic_widget"]
-
-
-@pytest.mark.parametrize("widget_name", MY_WIDGET_NAMES)
-def test_something_with_viewer(widget_name, make_napari_viewer, napari_plugin_manager):
-    napari_plugin_manager.register({{cookiecutter.module_name}}, name=MY_PLUGIN_NAME)
+# make_napari_viewer is a pytest fixture that returns a napari viewer object
+# capsys is a pytest fixture that captures stdout and stderr output streams
+def test_example_q_widget(make_napari_viewer, capsys):
+    # make viewer and add an image layer using our fixture
     viewer = make_napari_viewer()
-    num_dw = len(viewer.window._dock_widgets)
-    viewer.window.add_plugin_dock_widget(
-        plugin_name=MY_PLUGIN_NAME, widget_name=widget_name
-    )
-    assert len(viewer.window._dock_widgets) == num_dw + 1
+    viewer.add_image(np.random.random((100, 100)))
+
+    # create our widget, passing in the viewer
+    my_widget = ExampleQWidget(viewer)
+
+    # call our widget method
+    my_widget._on_click()
+
+    # read captured output and check that it's as we expected
+    captured = capsys.readouterr()
+    assert captured.out == "napari has 1 layers\n"
+    
+def test_example_magic_widget(make_napari_viewer, capsys):
+    viewer = make_napari_viewer()
+    layer = viewer.add_image(np.random.random((100, 100)))
+
+    # this time, our widget will be a MagicFactory or FunctionGui instance
+    my_widget = example_magic_widget()
+
+    # if we "call" this object, it'll execute our function
+    my_widget(viewer.layers[0])
+
+    # read captured output and check that it's as we expected
+    captured = capsys.readouterr()
+    assert captured.out == f"you have selected {layer}\n"
