bf1def038a3e320da9e6ad7d348f023c5c5c8459
Draga Doncila Pop <17995243+DragaDoncila@users.noreply.github.com>
2022-01-11 00:20:27 +1000

    Tweak `dock_widget` tests to get rid of `add_plugin_dock_widget` (#82) (#83)
    * Update widget tests to forego use of add_dock_widget
    
    * Add explanatory comments
    
    * Remove gratuitous image fixture

--- {{cookiecutter.plugin_name}}/src/{{cookiecutter.module_name}}/__init__.py
+++ {{cookiecutter.plugin_name}}/src/{{cookiecutter.module_name}}/__init__.py
@@ -10,4 +10,4 @@ __version__ = "0.0.1"
 {% if cookiecutter.include_reader_plugin == 'y' %}from ._reader import napari_get_reader{% endif %}
 {% if cookiecutter.include_writer_plugin == 'y' %}from ._writer import write_single_image, write_multiple {% endif %}
 {% if cookiecutter.include_sample_data_plugin == 'y' %}from ._sample_data import make_sample_data {% endif %}
-{% if cookiecutter.include_widget_plugin == 'y' %}from ._widget import ExampleQWidget{% endif %}
+{% if cookiecutter.include_widget_plugin == 'y' %}from ._widget import ExampleQWidget, example_magic_widget{% endif %}
