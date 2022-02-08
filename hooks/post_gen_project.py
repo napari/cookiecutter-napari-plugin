2c1a3ffb67b29f2630dc1bde587126b3ae543242
Adam Tyson <code@adamltyson.com>
2022-02-08 14:26:15 +0000

    add git init

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,17 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 import shutil
+import subprocess
 
 if __name__ == "__main__":
     shutil.rmtree("licenses", ignore_errors=True)
+
+    msg = ''
+    # try to run git init
+    try:
+        subprocess.run(["git", "init", "-q"])
+        subprocess.run(["git", "checkout", "-b", "main"])
+        subprocess.run(["git", "add", "."])
+        subprocess.run(["git", "commit", "-q", "-m", "initial commit"])
+    except Exception:
+        msg += ''
