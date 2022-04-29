4b171eb525ddeca8a7bf5088203af4381f92cc3a
cnstt <104427648+cnstt@users.noreply.github.com>
2022-04-29 14:29:23 +0200

    Update _sample_data.py
    Update broken link

--- {{cookiecutter.plugin_name}}/src/{{cookiecutter.module_name}}/_sample_data.py
+++ {{cookiecutter.plugin_name}}/src/{{cookiecutter.module_name}}/_sample_data.py
@@ -2,7 +2,7 @@
 This module is an example of a barebones sample data provider for napari.
 
 It implements the "sample data" specification.
-see: https://napari.org/plugins/stable/guides.html#sample-data
+see: https://napari.org/plugins/guides.html?highlight=sample_data#sample-data
 
 Replace code below according to your needs.
 """
