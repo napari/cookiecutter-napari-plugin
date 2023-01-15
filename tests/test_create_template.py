9dbd0b882a9e1c1d7f078bcbd924dceec59a85fe
Grzegorz Bokota <bokota+github@gmail.com>
2023-01-15 18:14:19 +0000

    fix tox call order

--- tests/test_create_template.py
+++ tests/test_create_template.py
@@ -14,7 +14,7 @@ def run_tox(plugin):
     """Run the tox suite of the newly created plugin."""
     try:
         subprocess.check_call(
-            ["tox", plugin, "-c", os.path.join(plugin, "tox.ini"), "-e", "py"]
+            ["tox", "-c", os.path.join(plugin, "tox.ini"), "-e", "py", "--", plugin]
         )
     except subprocess.CalledProcessError as e:
         pytest.fail("Subprocess fail", pytrace=True)
