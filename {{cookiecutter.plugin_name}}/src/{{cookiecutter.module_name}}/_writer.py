4204d9998468eabc1496f6e09b0f901e4bf31aa4
Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
2023-03-17 16:01:43 +1100

    Add @czaki's explanation of the meta dictionary

--- {{cookiecutter.plugin_name}}/src/{{cookiecutter.module_name}}/_writer.py
+++ {{cookiecutter.plugin_name}}/src/{{cookiecutter.module_name}}/_writer.py
@@ -21,12 +21,16 @@ def write_single_image(path: str, data: Any, meta: dict) -> List[str]:
     Parameters
     ----------
     path : str
-    data : The layer data attribute
+           A string path indicating where to save the image file.
+    data : The layer data
+           The `.data` attribute from the napari layer.
     meta : dict
+           A dictionary containing all other attributes from the napari layer
+           (excluding the `.data` layer attribute).
 
     Returns
     -------
-    [path] : A list containing the string path to the saved file
+    [path] : A list containing the string path to the saved file.
     """
 
     # implement your writer logic here ...
@@ -41,16 +45,16 @@ def write_multiple(path: str, data: List[FullLayerData]) -> List[str]:
     Parameters
     ----------
     path : str
+           A string path indicating where to save the image file.
     data : A layer tuple.
            Tuple contains three elements: (data, kwargs, layer_type)
            `data` is the layer data
            `kwargs` is a dictionary of keyword arguments
            `layer_type` is a string, eg: "image", "labels", "surface", etc.
-    meta : dict
 
     Returns
     -------
-    [path] : A list containing multiple string paths to the saved files
+    [path] : A list containing multiple string paths to the saved files.
     """
 
     # implement your writer logic here ...
