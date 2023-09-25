3b04070c7a8f9f530f92a817901cba3e12b828ad
Draga Doncila Pop <ddoncila@gmail.com>
2023-09-25 18:05:40 +1000

    Add container example and more docs

--- {{cookiecutter.plugin_name}}/src/{{cookiecutter.module_name}}/_widget.py
+++ {{cookiecutter.plugin_name}}/src/{{cookiecutter.module_name}}/_widget.py
@@ -1,28 +1,121 @@
 """
-This module is an example of a barebones QWidget plugin for napari
+This module contains four napari widgets declared in 
+different ways:
 
-It implements the Widget specification.
-see: https://napari.org/stable/plugins/guides.html?#widgets
+- a pure Python function flagged with `autogenerate: true`
+    in the plugin manifest. Type annotations are used by
+    magicgui to generate widgets for each parameter. Best
+    suited for simple processing tasks - usually taking
+    in and/or returning a layer.
+- a `magic_factory` decorated function. The `magic_factory`
+    decorator allows us to customize aspects of the resulting
+    GUI, including the widgets associated with each parameter.
+- a `magicgui.widgets.Container` subclass. This provides lots
+    of flexibility and customization options while still supporting
+    `magicgui` widgets and convenience methods for creating widgets
+    from type annotations. 
+- a `QWidget` subclass. This provides maximal flexibility but requires
+    full specification of widget layouts, callbacks, events, etc.
+
+References:
+- Widget specification: https://napari.org/stable/plugins/guides.html?#widgets
+- magicgui docs: https://pyapp-kit.github.io/magicgui/
 
 Replace code below according to your needs.
 """
 from typing import TYPE_CHECKING
 
 from magicgui import magic_factory
+from magicgui.widgets import Container, CheckBox, create_widget
+import numpy as np
 from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
 
 if TYPE_CHECKING:
     import napari
 
 
+# Uses the `autogenerate: true` flag in the plugin manifest
+# to indicate it should be wrapped as a magicgui to autogenerate
+# a widget.
+def example_function_widget(
+    img_layer: "napari.layers.Image",
+) -> "napari.types.LabelsData":
+    return img_layer.data > 0.5
+
+
+# the magic_factory decorator lets us customize aspects of our widget
+# we specify a widget type for the threshold parameter
+# and use auto_call=True so the function is called whenever
+# the value of a parameter changes
+@magic_factory(
+    threshold={"widget_type": "FloatSlider", "max": 20}, auto_call=True
+)
+def example_magic_widget(
+    img_layer: "napari.layers.Image", threshold: "float"
+) -> "napari.types.LabelsData":
+    return img_layer.data > threshold
+
+
+class ImageThreshold(Container):
+    def __init__(self, viewer: "napari.viewer.Viewer"):
+        super().__init__()
+        self._viewer = viewer
+        # use create_widget to generate widgets from type annotations
+        self._image_layer_combo = create_widget(
+            label="Image", annotation="napari.layers.Image"
+        )
+        self._threshold_slider = create_widget(
+            label="Threshold", annotation=float, widget_type="FloatSlider"
+        )
+        self._threshold_slider.min = 0
+        self._threshold_slider.max = 1
+        # use magicgui widgets directly
+        self._invert_checkbox = CheckBox(text="Keep pixels below threshold")
+
+        # connect your own callbacks
+        self._image_layer_combo.changed.connect(self._set_threshold_range)
+        self._threshold_slider.changed.connect(self._threshold_im)
+        self._set_threshold_range(self._image_layer_combo.value)
+
+        # append into/extend the container with your widgets
+        self.extend(
+            [
+                self._image_layer_combo,
+                self._threshold_slider,
+                self._invert_checkbox,
+            ]
+        )
+
+    def _set_threshold_range(self, im_layer: "napari.layers.Image"):
+        if im_layer is not None:
+            im_min = np.min(im_layer.data)
+            im_max = np.max(im_layer.data)
+            self._threshold_slider.min = im_min
+            self._threshold_slider.max = im_max
+
+    def _threshold_im(self, threshold: float):
+        image_layer = self._image_layer_combo.value
+        if image_layer is None:
+            return
+
+        image = image_layer.data
+        name = image_layer.name + "_thresholded"
+        if self._invert_checkbox.value:
+            thresholded = image < threshold
+        else:
+            thresholded = image > threshold
+        if name in self._viewer.layers:
+            self._viewer.layers[name].data = thresholded
+        else:
+            self._viewer.add_labels(thresholded, name=name)
+
+
 class ExampleQWidget(QWidget):
     # your QWidget.__init__ can optionally request the napari viewer instance
-    # in one of two ways:
-    # 1. use a parameter called `napari_viewer`, as done here
-    # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-    def __init__(self, napari_viewer):
+    # use a type annotation of 'napari.viewer.Viewer' for any parameter
+    def __init__(self, viewer: "napari.Viewer"):
         super().__init__()
-        self.viewer = napari_viewer
+        self.viewer = viewer
 
         btn = QPushButton("Click me!")
         btn.clicked.connect(self._on_click)
@@ -32,15 +125,3 @@ class ExampleQWidget(QWidget):
 
     def _on_click(self):
         print("napari has", len(self.viewer.layers), "layers")
-
-
-@magic_factory
-def example_magic_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
-
-
-# Uses the `autogenerate: true` flag in the plugin manifest
-# to indicate it should be wrapped as a magicgui to autogenerate
-# a widget.
-def example_function_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
