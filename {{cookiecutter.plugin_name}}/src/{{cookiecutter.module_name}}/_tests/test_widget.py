2799c847fe51f4c542d2d2daa4f941ade0ecf20b
Draga Doncila Pop <ddoncila@gmail.com>
2023-10-06 12:00:34 -0700

    Document the test

--- {{cookiecutter.plugin_name}}/src/{{cookiecutter.module_name}}/_tests/test_widget.py
+++ {{cookiecutter.plugin_name}}/src/{{cookiecutter.module_name}}/_tests/test_widget.py
@@ -37,7 +37,13 @@ def test_image_threshold_widget(make_napari_viewer):
     viewer = make_napari_viewer()
     layer = viewer.add_image(np.random.random((100, 100)))
     my_widget = ImageThreshold(viewer)
+
+    # because we saved our widgets as attributes of the container
+    # we can set their values without having to "interact" with the viewer
     my_widget._image_layer_combo.value = layer
+
+    # this allows us to run our functions directly and ensure
+    # correct results
     my_widget._threshold_im(0.5)
     assert len(viewer.layers) == 2
 
