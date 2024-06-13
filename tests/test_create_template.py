80adc478edcba0e5202481ac391879b1dc04b653
Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
2024-06-13 18:11:38 +1000

    Remove references to cookiecutter and pytest-cookies

--- tests/test_create_template.py
+++ tests/test_create_template.py
@@ -19,61 +19,82 @@ def run_tox(plugin):
     except subprocess.CalledProcessError as e:
         pytest.fail("Subprocess fail", pytrace=True)
 
-@pytest.mark.parametrize("include_reader_plugin", ["y", "n"])
-@pytest.mark.parametrize("include_writer_plugin", ["y", "n"])
-@pytest.mark.parametrize("include_sample_data_plugin", ["y", "n"])
-@pytest.mark.parametrize("include_widget_plugin", ["y", "n"])
-def test_run_cookiecutter_and_plugin_tests(cookies, capsys, include_reader_plugin, include_writer_plugin, include_sample_data_plugin, include_widget_plugin):
-    """Create a new plugin via cookiecutter and run its tests."""
-    result = cookies.bake(extra_context={
-            "plugin_name": "foo-bar",
-            "include_reader_plugin": include_reader_plugin,
-            "include_writer_plugin": include_writer_plugin,
-            "include_sample_data_plugin": include_sample_data_plugin,
-            "include_widget_plugin": include_widget_plugin,
-        }
-    )
+
+@pytest.mark.parametrize("include_reader_plugin", [True, False])
+@pytest.mark.parametrize("include_writer_plugin", [True, False])
+@pytest.mark.parametrize("include_sample_data_plugin", [True, False])
+@pytest.mark.parametrize("include_widget_plugin", [True, False])
+def test_run_plugin_tests(copie, capsys, include_reader_plugin, include_writer_plugin, include_sample_data_plugin, include_widget_plugin):
+    """Create a new plugin with the napari plugin template and run its tests."""
+    result = copie.copy(extra_answers={
+        "plugin_name": "foo-bar",
+        "display_name": "Foo Bar",
+        "short_description": "Super fast foo for all the bars",
+        "full_name": "napari bot",
+        "email": "etal@example.com",
+        "github_username_or_organization": "napari",
+        "include_reader_plugin": include_reader_plugin,
+        "include_writer_plugin": include_writer_plugin,
+        "include_sample_data_plugin": include_sample_data_plugin,
+        "include_widget_plugin": include_widget_plugin,
+        })
 
     assert result.exit_code == 0
     assert result.exception is None
-    assert result.project_path.name == "foo-bar"
-    assert result.project_path.is_dir()
-    assert result.project_path.joinpath("src").is_dir()
-    assert result.project_path.joinpath("src", "foo_bar", "__init__.py").is_file()
+    assert result.project_dir.is_dir()
+    with open(result.project_dir/"README.md") as f:
+        assert f.readline() == "# foo-bar\n"
+    assert result.project_dir.joinpath("src").is_dir()
+    assert result.project_dir.joinpath("src", "foo_bar", "__init__.py").is_file()
 
-    test_path = result.project_path.joinpath("src", "foo_bar", "_tests")
-    if include_reader_plugin == "y":
+    test_path = result.project_dir.joinpath("src", "foo_bar", "_tests")
+    if include_reader_plugin is True:
         assert (test_path / "test_reader.py").is_file()
-    if include_writer_plugin == "y":
+    if include_writer_plugin is True:
         assert (test_path / "test_writer.py").is_file()
-    if include_sample_data_plugin == "y":
+    if include_sample_data_plugin is True:
         assert (test_path / "test_sample_data.py").is_file()
-    if include_widget_plugin == "y":
+    if include_widget_plugin is True:
         assert (test_path / "test_widget.py").is_file()
 
-    # if all are `n` there are no modules or tests
-    if "y" in {include_reader_plugin, include_writer_plugin, include_sample_data_plugin, include_widget_plugin}:
-        run_tox(str(result.project_path))
+    # if all are False there are no modules or tests
+    if True in {include_reader_plugin, include_writer_plugin, include_sample_data_plugin, include_widget_plugin}:
+        run_tox(str(result.project_dir))
 
 
-def test_run_cookiecutter_and_plugin_tests_with_napari_prefix(cookies, capsys):
+def test_run_plugin_tests_with_napari_prefix(copie, capsys):
     """make sure it's also ok to use napari prefix."""
-    result = cookies.bake(extra_context={"plugin_name": "napari-foo"})
+    name = "napari-foo"
+    result = copie.copy(extra_answers={
+        "plugin_name": name,
+        "display_name": "napari Foo",
+        "short_description": "Super fast foo for all the bars",
+        "full_name": "napari bot",
+        "email": "etal@example.com",
+        "github_username_or_organization": "napari",
+        })
 
     assert result.exit_code == 0
     assert result.exception is None
-    assert result.project_path.name == "napari-foo"
-    assert result.project_path.is_dir()
-    assert result.project_path.joinpath("src").is_dir()
-    assert result.project_path.joinpath("src", "napari_foo", "__init__.py").is_file()
-    assert result.project_path.joinpath("src", "napari_foo", "_tests", "test_reader.py").is_file()
+    assert result.project_dir.is_dir()
+    with open(result.project_dir/"README.md") as f:
+        assert f.readline() == f"# {name}\n"
+    assert result.project_dir.joinpath("src").is_dir()
+    assert result.project_dir.joinpath("src", "napari_foo", "__init__.py").is_file()
+    assert result.project_dir.joinpath("src", "napari_foo", "_tests", "test_reader.py").is_file()
 
 
-def test_run_cookiecutter_select_plugins(cookies, capsys):
+def test_run_select_plugins(copie, capsys):
     """make sure it's also ok to use napari prefix."""
-    result = cookies.bake(
-        extra_context={
-            "plugin_name": "anything",
+    name = "anything"
+    result = copie.copy(
+        extra_answers={
+            "plugin_name": name,
+            "display_name": "Foo Bar",
+            "short_description": "Super fast foo for all the bars",
+            "full_name": "napari bot",
+            "email": "etal@example.com",
+            "github_username_or_organization": "napari",
             "include_widget_plugin": "n",
             "include_writer_plugin": "n",
         }
@@ -81,35 +102,41 @@ def test_run_cookiecutter_select_plugins(cookies, capsys):
 
     assert result.exit_code == 0
     assert result.exception is None
-    assert result.project_path.name == "anything"
-    assert result.project_path.is_dir()
-    assert result.project_path.joinpath("src").is_dir()
-    assert result.project_path.joinpath("src", "anything", "__init__.py").is_file()
-    assert result.project_path.joinpath("src", "anything", "_tests", "test_reader.py").is_file()
+    assert result.project_dir.is_dir()
+    with open(result.project_dir/"README.md") as f:
+        assert f.readline() == f"# {name}\n"
+    assert result.project_dir.joinpath("src").is_dir()
+    assert result.project_dir.joinpath("src", name, "__init__.py").is_file()
+    assert result.project_dir.joinpath("src", name, "_tests", "test_reader.py").is_file()
 
-    assert not result.project_path.joinpath("src", "anything", "_widget.py").is_file()
-    assert not result.project_path.joinpath(
+    assert not result.project_dir.joinpath("src", "anything", "_widget.py").is_file()
+    assert not result.project_dir.joinpath(
         "src", "anything", "_tests", "test_widget.py"
     ).is_file()
-    assert not result.project_path.joinpath("src", "anything", "_writer.py").is_file()
-    assert not result.project_path.joinpath(
+    assert not result.project_dir.joinpath("src", "anything", "_writer.py").is_file()
+    assert not result.project_dir.joinpath(
         "src", "anything", "_tests", "test_writer.py"
     ).is_file()
 
 
-@pytest.mark.parametrize("include_reader_plugin", ["y", "n"])
-@pytest.mark.parametrize("include_writer_plugin", ["y", "n"])
-@pytest.mark.parametrize("include_sample_data_plugin", ["y", "n"])
-@pytest.mark.parametrize("include_widget_plugin", ["y", "n"])
-def test_pre_commit_validity(cookies, include_reader_plugin, include_writer_plugin, include_sample_data_plugin, include_widget_plugin):
-    result = cookies.bake(
-        extra_context={
+@pytest.mark.parametrize("include_reader_plugin", [True, False])
+@pytest.mark.parametrize("include_writer_plugin", [True, False])
+@pytest.mark.parametrize("include_sample_data_plugin", [True, False])
+@pytest.mark.parametrize("include_widget_plugin", [True, False])
+def test_pre_commit_validity(copie, include_reader_plugin, include_writer_plugin, include_sample_data_plugin, include_widget_plugin):
+    result = copie.copy(
+        extra_answers={
             "plugin_name": "anything",
+            "display_name": "Foo Bar",
+            "short_description": "Super fast foo for all the bars",
+            "full_name": "napari bot",
+            "email": "etal@example.com",
+            "github_username_or_organization": "napari",
             "include_reader_plugin": include_reader_plugin,
             "include_writer_plugin": include_writer_plugin,
             "include_sample_data_plugin": include_sample_data_plugin,
             "include_widget_plugin": include_widget_plugin,
-            "install_precommit": "y",
+            "install_precommit": True,
         }
     )
     result.project_path.joinpath("setup.cfg").is_file()
